a1f3924a7f02ba4cda8e05b1d9c69e3c0ccd5fe1
utukJ <utukphd@gmail.com>
2021-04-27 22:31:31 +0100

    password reset

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,4 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
